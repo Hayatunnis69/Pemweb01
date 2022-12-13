919275e1fe486a08cc441f65c9ed2e8cca4d7535
Hayatunnis69 <114377124+Hayatunnis69@users.noreply.github.com>
2022-12-13 10:18:53 +0700

    Array & Object

---
+++ p11_Hayatunnisa_TI05/assets/js/script.js
@@ -0,0 +1,46 @@
+document.querySelector("#option a").forEach((a)=> {
+    // jika di klik akan menjalankan fungsi coputerchoise()
+    a.addEventListener("click", (element) => {
+        computerchoise(element);
+    })
+})
+function computerchoise(element) {
+    // tangkap pilihan user
+    let pilihanUser = element.target.innerText;
+
+    // menangkap element result dengan queryselector untuk menampung 
+    // nilai hasil dari game
+    let pilihanComputer = document.querySelector("#result");
+
+    // membuat array pilihan umtuk komputer
+    let choise = ["Rock", "Paper", "Scissors"];
+
+    // pilihan random untuk komputer
+    pilihanComputer.innerHTML = choise[Math.round(Math.random() * choise.length)] 
+    pilihanComputer = pilihanComputer.innerHTML;
+
+    // jika pilihan komputer sama dengan pilihan user(Draw)
+    if (pilihanUser == pilihanComputer) {
+        alert("DRAW");
+    }
+
+    // Jika pilihan user yang menang
+    if (pilihanUser == "Rock" && pilihanComputer == "Scissors") {
+        alert("YOU WIN");
+    }else if(pilihanUser == "Paper" && pilihanComputer == "Rock") {
+        alert("YOU WIN");
+    }else if(pilihanUser == "Scissors" && pilihanComputer == "Paper") {
+        alert("YOU WIN");
+    }else{
+        alert("Tidak ada pilihan");
+    }
+
+    // jika pilihan komputer yang menang
+    if (pilihanUser == "Scissors" && pilihanComputer == "Rock") {
+        alert("COMPUTER WIN");
+    }else if (pilihanUser == "Paper" && pilihanComputer == "Scissors") {
+        alert("COMPUTER WIN");
+    }else if (pilihanUser == "Scissors" && pilihanComputer == "Rock") {
+        alert("COMPUTER WIN");
+    }
+}
